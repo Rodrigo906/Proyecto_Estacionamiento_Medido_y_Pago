7defeb5ae7b61e4b35d294f32518f461dc6caf3f
sebatraipe <traipesebastian@gmail.com>
2021-10-11 10:31:24 -0300

    agregué un dato de prueba

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,6 +7,7 @@ class Home extends BaseController
     public function index()
     {
         print_r("Hola..");
+        print_r("Datos de prueba...");
         return view('welcome_message');
     }
 }
