ede1db3a98e2eee2f5d6f7e7d53f9659e1e0da24
sebatraipe <traipesebastian@gmail.com>
2021-10-11 12:07:53 -0300

    Elimine los print

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,8 +6,6 @@ class Home extends BaseController
 {
     public function index()
     {
-        print_r("Hola..");
-        print_r("Datos de prueba...");
         return view('welcome_message');
     }
 }
