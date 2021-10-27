00c23af9fef54edf54f227bcd670cb21f0370073
sebatraipe <traipesebastian@gmail.com>
2021-10-27 13:10:39 -0300

    Commit de prueba

--- app/Controllers/User_controller.php
+++ app/Controllers/User_controller.php
@@ -1,16 +1,19 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Models\UserModel;
 use App\Models\RolModel;
 
 
-class User_controller extends BaseController{
+class User_controller extends BaseController
+{
 
     protected $userModel;
     protected $rolModel;
-    
-    public function __construct(){
+
+    public function __construct()
+    {
 
         $this->userModel = new UserModel();
         $this->rolModel = new RolModel();
@@ -18,26 +21,30 @@ class User_controller extends BaseController{
 
 
     //retorna el listado de todos los usuarios del sistema
-    public function index(){
+    public function index()
+    {
 
         $data['usuarios'] = $this->userModel->obtenerListadoUsuaurios();
-        return view('vistas_administrador/listado_usuarios', $data);  
+        return view('vistas_administrador/listado_usuarios', $data);
+        print_r("Hola");
     }
 
     //Al llamarlo mostrara el formulario de registro de usuario
-    public function MostrarFormularioRegistro (){
+    public function MostrarFormularioRegistro()
+    {
         return view('vistas_administrador/formulario_registro_usuario');
     }
 
     //tomara los datos del formulario y los guardara en la BD
-    public function registrarUsuario(){
-        
+    public function registrarUsuario()
+    {
+
         /* PARA PRUEBAS
         $valor1 = $_POST['valor1'];
         $valor2 = $_POST['valor2'];
         echo $valor1 ."---". $valor2;
         */
-        
+
         $username = $_POST['username'];
         $nombre = $_POST['nombre'];
         $apellido = $_POST['apellido'];
@@ -47,20 +54,26 @@ class User_controller extends BaseController{
         $contraseña = $_POST['contraseña'];
         $rol = $this->rolModel->find($_POST['rol']);
 
-        $this->userModel->registrarUsuario($username, $nombre, $apellido, $email, $dni, 
-                                                    $fecha_nacimiento, $contraseña, $rol);
-                                                
+        $this->userModel->registrarUsuario(
+            $username,
+            $nombre,
+            $apellido,
+            $email,
+            $dni,
+            $fecha_nacimiento,
+            $contraseña,
+            $rol
+        );
     }
 
-    
+
     //Elimina cualquier usuario pasado por parametro
-    public function eliminar ($id_usuario){
+    public function eliminar($id_usuario)
+    {
 
         $this->userModel->delete($id_usuario);
         $data['usuarios'] = $this->userModel->obtenerListadoUsuaurios();
 
-        return view('vistas_administrador/listado_usuarios', $data);  
+        return view('vistas_administrador/listado_usuarios', $data);
     }
 }
-
-?>
