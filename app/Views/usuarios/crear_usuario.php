9b873ed2ab2f2498572dbef3532f1273e11d6973
Rodrigo906 <82407521+Rodrigo906@users.noreply.github.com>
2021-10-28 17:20:16 -0300

    Correcciones

--- app/Views/usuarios/crear_usuario.php
+++ app/Views/usuarios/crear_usuario.php
@@ -37,7 +37,7 @@
                                 <h1 class="h4 text-gray-900 mb-4"> <?= $subtitulo ?> </h1>
                             </div>
 
-                            <form class="user" method="POST" action="<?= base_url('registrarUsuario') ?>">
+                            <form class="user" method="POST" action="<?= base_url('User_controller/registrarUsuario') ?>">
                                 <div class="form-group row">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
                                         <input type="text" class="form-control form-control-user" id="nombre" placeholder="Nombre" name="nombre">
--- app/Views/usuarios/listado_usuarios.php
+++ app/Views/usuarios/listado_usuarios.php
@@ -5,7 +5,7 @@
 <div class="contenedor_tabla" style="margin: 20px;">
 
     <div class="content-icono">
-        <a href="MostrarFormularioRegistro" data-toggle="tooltip" title="Agregar">
+        <a href="<?= base_url('User_controller/MostrarFormularioRegistro')?>" data-toggle="tooltip" title="Agregar">
             <i class="bi bi-plus-circle-fill" style="font-size: 2rem;"></i>
         </a>
 
